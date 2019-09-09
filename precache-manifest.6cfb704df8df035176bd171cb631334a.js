55bf25784e0686f58fc9199fc919404812ae18a1
magna25 <hennymaster1@gmail.com>
2019-09-09 01:15:55 -0600

    Updates

---
+++ precache-manifest.6cfb704df8df035176bd171cb631334a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80d3b282e7d78fff4228641c214faf7",
+    "url": "/lottie-editor/index.html"
+  },
+  {
+    "revision": "22512adc5481c0f1a967",
+    "url": "/lottie-editor/static/css/main.ace4c10c.chunk.css"
+  },
+  {
+    "revision": "acf45804ea5efea80e03",
+    "url": "/lottie-editor/static/js/2.10bd8c09.chunk.js"
+  },
+  {
+    "revision": "22512adc5481c0f1a967",
+    "url": "/lottie-editor/static/js/main.2c4d4a02.chunk.js"
+  },
+  {
+    "revision": "f185a99f6e419ef95183",
+    "url": "/lottie-editor/static/js/runtime~main.0f5720fa.js"
+  }
+]);
